f632dd8f8ba5b9f0e0d65aea6bca3de1ce08a52b
Luke Chalkley <lukechalkley@hotmail.com.au>
2026-02-28 22:21:59 +1100

    Created AppUser entity and initial migration.

---
+++ DatingApp/API/Entities/AppUser.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    /// <summary>
+    /// Gets the ID of the AppUser.
+    /// This is set to a string representation of a Guid when AppUser is instantiated.
+    /// </summary>
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    
+    /// <summary>
+    /// Gets or sets the display name of the AppUser instance.
+    /// </summary>
+    public required string DisplayName { get; set; }
+    
+    /// <summary>
+    /// Gets or sets e-mail of the AppUser instance.
+    /// </summary>
+    public required string Email { get; set; }
+}
--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,3 +1,6 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>
+(opt =>
+    {
+        opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    }
+);
 
 var app = builder.Build();
 
